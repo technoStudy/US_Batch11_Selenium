7b73fd5a63e116366b5913517ce0842feff5fa09
mehmet ali sezgin <49106964+mehmetalisezgin@users.noreply.github.com>
2024-01-24 20:50:59 +0100

    updated

--- src/day07/C01_ActionsClass.java
+++ src/day07/C01_ActionsClass.java
@@ -71,6 +71,55 @@ public class C01_ActionsClass extends TestBase {
         // 4-Confirm that the message
         WebElement rihtClickMessage = driver.findElement(By.xpath("//p[@id='rightClickMessage']"));
         Assert.assertTrue(rihtClickMessage.isDisplayed());
+
+    }
+
+    /*
+    TC ID: TC_DEMOQA_004
+    TC Title: Verify Hover Over Functionality on 'Menu' Widget in DemoQA Website
+    TC Description: This test case ensures that a user can navigate to the 'Menu'
+    widget on demoqa.com, hover over 'Main Item 2', then 'SUB List', and finally
+    verify if 'Sub Item 1' is visible.
+    Test Steps:
+    1-Launch the Chrome browser and navigate to https://demoqa.com
+    2-On the homepage, click on the 'Widgets' button.
+    3-On the Widgets page, click on the 'Menu' button.
+    4-Hover over the 'Main Item 2' to trigger the first popup.
+    5-Move the mouse to hover over the 'SUB List' to trigger the second popup.
+    6-Verify that 'Sub Item 1' becomes visible.
+    Expected Result: When the mouse is hovered over 'Main Item 2', a popup
+    should appear. As the mouse moves over 'SUB List', a new popup should appear,
+    and 'Sub Item 1' should be visible, indicating the hover functionality is working as expected.
+
+     */
+
+    @Test
+    public void hoverOverTest() {
+        // 2-On the homepage, click on the 'Widgets' button.
+        WebElement widgetButton =
+                driver.findElement(By.xpath("(//div[@class='avatar mx-auto white'])[4]"));
+        ReusableMethods.myWait(2);
+        widgetButton.click();
+        // 3-On the Widgets page, click on the 'Menu' button.
+        WebElement menuButton =
+                driver.findElement(By.xpath("(//li[@id='item-7'])[2]"));
+        menuButton.click();
+        // 4-Hover over the 'Main Item 2' to trigger the first popup.
+        WebElement mainItem2Button = driver.findElement(By.xpath("(//a[@href='#'])[2]"));
+        ReusableMethods.myWait(2);
+        Actions actions = new Actions(driver);
+        actions.moveToElement(mainItem2Button).build().perform();
+        //5-Move the mouse to hover over the 'SUB List' to trigger the second popup.
+        ReusableMethods.myWait(2);
+        WebElement subListItem = driver.findElement(By.xpath("(//a[@href='#'])[5]"));
+        actions.moveToElement(subListItem).build().perform();
+
+        // 6-Verify that 'Sub Item 1' becomes visible.
+        ReusableMethods.myWait(2);
+        WebElement subItem1 = driver.findElement(By.xpath("(//a[@href='#'])[6]"));
+        actions.moveToElement(subItem1).build().perform();
+        Assert.assertTrue(subItem1.isDisplayed());
+
     }
 
     @After
